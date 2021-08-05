8a47c1d9d5d72889068a1feb8d27a28cab89f26f
Jeff Free <jefffreelance3@gmail.com>
2021-08-04 21:19:58 -0300

    mejorado diseño

--- src/script/arbitraje.js
+++ src/script/arbitraje.js
@@ -4,14 +4,19 @@ let redWarning = 0;
 let blueWarning = 0;
 let redDiscount = 0;
 let blueDiscount = 0;
+let fightStatus = "ended";
 
 function startFight() {
 	const resultRed = document.getElementById("resultadoRed");
 	const resultBlue = document.getElementById("resultadoBlue");
+	const status = document.getElementById("status");
+
 
 	resultRed.innerText = "";
 	resultBlue.innerText = "";
+	status.innerText = "Combate iniciado"
 
+	fightStatus = "running";
 	redTotal = 0;
 	blueTotal = 0;
 	redWarning = 0;
@@ -21,54 +26,66 @@ function startFight() {
 }
 
 function warning(color) {
-	if (color == "red") {
-		redWarning +=1;
-		if (redWarning == 3) {
-			redScore(-1);
-			redWarning = 0;
-		};
-	} else if (color == "blue") {
-		blueWarning +=1;
-		if (blueWarning == 3) {
-			blueScore(-1);
-			blueWarning = 0;
+	if (fightStatus == "running") {
+		if (color == "red") {
+			redWarning +=1;
+			if (redWarning == 3) {
+				redScore(-1);
+				redWarning = 0;
+			};
+		} else if (color == "blue") {
+			blueWarning +=1;
+			if (blueWarning == 3) {
+				blueScore(-1);
+				blueWarning = 0;
+			};
 		};
 	};
 }
 
 function discount(color) {
-	if (color == "red") {
-		redDiscount += 1;
-		redScore(-1);
-		if (redDiscount == 3) {
-			redTotal = "DQ";
-			endFight();
-		}
-	} else if (color == "blue") {
-		blueDiscount += 1;
-		blueScore(-1);
-		if (blueDiscount == 3) {
-			blueTotal = "DQ";
-			endFight();
-		}
-	}
+	if (fightStatus == "running") {
+		if (color == "red") {
+			redDiscount += 1;
+			redScore(-1);
+			if (redDiscount == 3) {
+				redTotal = "DQ";
+				endFight();
+			}
+		} else if (color == "blue") {
+			blueDiscount += 1;
+			blueScore(-1);
+			if (blueDiscount == 3) {
+				blueTotal = "DQ";
+				endFight();
+			};
+		};
+	};
 }
 
 
 function redScore(points) {
-	redTotal += points;
-	console.log(redTotal);
+	if (fightStatus == "running") {
+		redTotal += points;
+		console.log(redTotal);
+	};
 };
 
 function blueScore(points) {
-	blueTotal += points;
-	console.log(blueTotal);
+	if (fightStatus == "running") {
+		blueTotal += points;
+		console.log(blueTotal);
+	};
 };
 
 function endFight() {
 	const resultRed = document.getElementById("resultadoRed");
 	const resultBlue = document.getElementById("resultadoBlue");
+	const status = document.getElementById("status");
 
 	resultRed.innerText = redTotal;
 	resultBlue.innerText = blueTotal;
+	status.innerText = "Combate terminado"
+
+	fightStatus= "ended";
 };
